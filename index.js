d170433aaf065c32571e97691968a4b497217eed
Joe Dahlquist <joe@arcanyx.com>
2023-06-22 16:07:44 -0700

    Use new flat config for v2

--- index.js
+++ index.js
@@ -1,27 +1,46 @@
-module.exports = {
-    extends: [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:@typescript-eslint/recommended-requiring-type-checking",
-    ],
-    rules: {
-        // Require `const` instead of `let` when the variable is never reassigned.
-        "prefer-const": "error",
+import js from "@eslint/js";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
 
-        // Require double-quotes rather than single-quotes.
-        // Standard eslint quotes must be disabled when using Typescript version.
-        quotes: "off",
-        "@typescript-eslint/quotes": ["error"],
+export const tsNodeConfigs = [
+    js.configs.recommended,
+    {
+        files: ["**/*.ts", "**/*.tsx"],
+        languageOptions: {
+            parser: tsParser,
+            parserOptions: {
+                project: "./tsconfig.json"
+            }
+        },
+        plugins: {
+            "@typescript-eslint": tsPlugin
+        },
+        rules: {
+            // A wee hack since @typescript-eslint doesn't officially support flat ESLint config.
+            ...tsPlugin.configs["eslint-recommended"].overrides.reduce((a, b) =>
+                ({...a.rules, ...b.rules}), {}),
 
-        // Require semicolons at the end of all statements.
-        // Standard eslint semicolon enforcement must be disabled when using Typescript version.
-        semi: "off",
-        "@typescript-eslint/semi": ["error"],
+            ...tsPlugin.configs["recommended"].rules,
+            ...tsPlugin.configs["recommended-requiring-type-checking"].rules,
 
-        "no-unused-vars": "off",
-        "@typescript-eslint/no-unused-vars": ["error", {"argsIgnorePattern": "^_"}],
+            // Require `const` instead of `let` when the variable is never reassigned.
+            "prefer-const": "error",
 
-        // Require switch statements to cover every case or have a default case.
-        "@typescript-eslint/switch-exhaustiveness-check": "error",
-    }
-};
+            // Require double-quotes rather than single-quotes.
+            // Standard eslint quotes must be disabled when using Typescript version.
+            quotes: "off",
+            "@typescript-eslint/quotes": ["error"],
+
+            // Require semicolons at the end of all statements.
+            // Standard eslint semicolon enforcement must be disabled when using Typescript version.
+            semi: "off",
+            "@typescript-eslint/semi": ["error"],
+
+            "no-unused-vars": "off",
+            "@typescript-eslint/no-unused-vars": ["error", {"argsIgnorePattern": "^_"}],
+
+            // Require switch statements to cover every case or have a default case.
+            "@typescript-eslint/switch-exhaustiveness-check": "error",
+        }
+    },
+];
